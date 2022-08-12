c86da1efda5aea30382283c137b58234d149383c
sanguine98 <108263416+sanguine98@users.noreply.github.com>
2022-08-13 00:30:30 +0530

    Add files via upload

---
+++ inc/client.h
@@ -0,0 +1,22 @@
+#include <myHeader.h>
+#define PORT 40794
+
+int process_cdr();
+
+class User{
+        public:
+        User() {}
+        User(int a,string u,string g,string p){
+                age=a;
+            username=u;
+            gender=g;
+            password=p;
+        }
+        string getPass();
+        string getUser();
+        private:
+        int age;
+        string username;
+        string gender;
+        string password;
+};
---
+++ inc/myHeader.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h> /* for definition of errno */
+#include <stdarg.h> /* ISO C variable aruments */
+#include <cstring>
+#include <cstdlib>
+#include <map>
+#include <string>
+
+
+using namespace std;
+
+
+const std::string WHITESPACE = " \n\r\t\f\v";
+ 
+std::string ltrim(const std::string &s)
+{
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+ 
+std::string rtrim(const std::string &s)
+{
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+ 
+std::string trim(const std::string &s) {
+    return rtrim(ltrim(s));
+}
